977756a4d7fa67fa63032d5e44112de573e7d19e
chrissierra <christopher.sierra@usach.cl>
2019-01-12 19:13:12 -0300

    enviando lo q tengo ahora

--- index.ts
+++ index.ts
@@ -9,17 +9,20 @@ const mysql = MySQL.instance;
 // Parser
 
 
-server.app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
 
 
 server.app.use( bodyParser.urlencoded({extended:true}) );
 server.app.use(bodyParser.json());
 
-server.app.use(cors({origin: true , credentials:true}));
+server.app.use(cors({origin: true , credentials:false}));
+
+
+server.app.use(function(req, res, next){
+	res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
 
 server.app.use('/', router)
 
--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,5 +1,5 @@
 import {Socket} from 'socket.io';
-
+import socketIO from 'socket.io';
 
 
 export const desconectar = ( cliente:Socket ) => {
@@ -11,9 +11,13 @@ export const desconectar = ( cliente:Socket ) => {
 
 
 
-export const mensaje = ( cliente:Socket ) => {
+export const mensaje = ( cliente:Socket, io: socketIO.Server ) => {
 
-	cliente.on('mensaje',  (payload)=>{
-		console.log('mensaje recibido en servidor ', payload)
-	} )
+    cliente.on('mensaje', (  payload: { de: string, cuerpo: string }  ) => {
+
+        console.log('Mensaje recibido', payload );
+
+        io.emit('mensaje-nuevo', payload );
+
+    });
 }
