548e5756da840e1f4bff1ac02ea544baada5d366
chrissierra <christopher.sierra@usach.cl>
2019-01-12 17:55:40 -0300

    Agrege app use con asterisco

--- index.ts
+++ index.ts
@@ -7,6 +7,15 @@ const server = Server.instance;
 const mysql = MySQL.instance;
 //const mysql = new MySQL();
 // Parser
+
+
+server.app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 server.app.use( bodyParser.urlencoded({extended:true}) );
 server.app.use(bodyParser.json());
 
